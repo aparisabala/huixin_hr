1c8d1f88617fae6f908ecacb48bf5d46a3f31979
unknown <Paris Ariyan>
2026-01-28 15:52:50 +0600

    added

--- app/Http/Middleware/SetBootConfig.php
+++ app/Http/Middleware/SetBootConfig.php
@@ -8,6 +8,7 @@ use Closure;
 use Illuminate\Http\Request;
 use Symfony\Component\HttpFoundation\Response;
 use File;
+
 class SetBootConfig
 {
     use BaseTrait;
@@ -18,21 +19,22 @@ class SetBootConfig
      */
     public function handle(Request $request, Closure $next): Response
     {
-        $app_data = AppData::where('id',1)->select(['*'])->first();
-        if(!empty($app_data) ) {
+        $app_data = AppData::where('id', 1)->select(['*'])->first();
+        if (!empty($app_data)) {
             config(['a' => $app_data]);
-            config(['i' => [
-                    'service_name' => "Service Name",
-                    "service_domain"=> "domain.com",
-                    "address"=>"Address",
-                    "mobile_number" =>"01XXX0017454",
-                    "service_email" =>"email@domain.com",
-                    'logo'=>url('images/system/logo.png'),
-                    'favicon'=>url('images/system/favicon.png'),
+            config([
+                'i' => [
+                    'service_name' => "Huixinglobal Hrm",
+                    "service_domain" => "huixinglobal.shop",
+                    "address" => "Address",
+                    "mobile_number" => "01XXX0017454",
+                    "service_email" => "email@domain.com",
+                    'logo' => url('images/system/logo.png'),
+                    'favicon' => url('images/system/favicon.png'),
                 ]
             ]);
             foreach (uploadsDir() as $key => $path) {
-                if(!is_dir($path)) {
+                if (!is_dir($path)) {
                     File::makeDirectory($path, 0755, true);
                 }
             }
